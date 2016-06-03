d2108aa1d16ee6826bf0ea859953f3cd450a22eb
Eric Westenberger <eric.westenberger@gmail.com>
2016-06-03 11:35:40 +0200

    Fix Issue 656: reset input stream before retry

--- connectors/jersey2/src/main/java/org/openstack4j/connectors/jersey2/HttpCommand.java
+++ connectors/jersey2/src/main/java/org/openstack4j/connectors/jersey2/HttpCommand.java
@@ -14,8 +14,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.client.RequestEntityProcessing;
 import org.glassfish.jersey.client.HttpUrlConnectorProvider;
+import org.glassfish.jersey.client.RequestEntityProcessing;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.openstack4j.api.exceptions.ConnectionException;
 import org.openstack4j.core.transport.ClientConstants;
@@ -35,7 +35,7 @@ public final class HttpCommand<R> {
     private int retries;
 
     private HttpCommand(HttpRequest<R> request) {
-	this.request = request;
+        this.request = request;
     }
 
     /**
@@ -44,28 +44,28 @@ public final class HttpCommand<R> {
      * @return the command
      */
     public static <R> HttpCommand<R> create(HttpRequest<R> request) {
-	HttpCommand<R> command = new HttpCommand<R>(request);
-	command.initialize();
-	return command;
+        HttpCommand<R> command = new HttpCommand<R>(request);
+        command.initialize();
+        return command;
     }
 
     private void initialize() {
-	Client client = ClientFactory.create(request.getConfig());
-	//try to set unsupported HTTP method. In our case used for PATCH.
-	if( request.getMethod().name()=="PATCH" )
-	    client.property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true);
+        Client client = ClientFactory.create(request.getConfig());
+        //try to set unsupported HTTP method. In our case used for PATCH.
+        if( request.getMethod().name()=="PATCH" )
+            client.property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true);
+        
+        WebTarget target = client.target(request.getEndpoint()).path(request.getPath());
+        
+        if (HttpLoggingFilter.isLoggingEnabled())
+            target.register(new LoggingFilter(Logger.getLogger("os"), 10000));
 
-	WebTarget target = client.target(request.getEndpoint()).path(request.getPath());
+        target = populateQueryParams(target, request);
+        invocation = target.request(MediaType.APPLICATION_JSON);
+        
+        populateHeaders(invocation,  request);
 
-	if (HttpLoggingFilter.isLoggingEnabled())
-	    target.register(new LoggingFilter(Logger.getLogger("os"), 10000));
-
-	target = populateQueryParams(target, request);
-	invocation = target.request(MediaType.APPLICATION_JSON);
-
-	populateHeaders(invocation,  request);
-
-	entity = (request.getEntity() == null) ? null : Entity.entity(request.getEntity(), request.getContentType());
+        entity = (request.getEntity() == null) ? null : Entity.entity(request.getEntity(), request.getContentType());
     }
 
     /**
@@ -74,96 +74,96 @@ public final class HttpCommand<R> {
      * @return the response
      */
     public Response execute() {
-	Response response = null;
+        Response response = null;
 
-	if (hasEntity()) {
-	    if (isInputStreamEntity())
-	    {             
-		// Issue #20 - Out of Memory in Jersey for large streams
-		invocation.property(ClientProperties.CHUNKED_ENCODING_SIZE, 1024);
-		invocation.property(ClientProperties.REQUEST_ENTITY_PROCESSING, RequestEntityProcessing.CHUNKED);
-	    }
-	    response = invocation.method(request.getMethod().name(), getEntity());
-	}
-	else if(request.hasJson()) {
-	    response = invocation.method(request.getMethod().name(), Entity.entity(request.getJson(), ClientConstants.CONTENT_TYPE_JSON));
-	}
-	else
-	{
-	    response = invocation.method(request.getMethod().name());
-	}
-
-	return response;
+        if (hasEntity()) {
+            if (isInputStreamEntity())
+            {
+                // Issue #20 - Out of Memory in Jersey for large streams
+                invocation.property(ClientProperties.CHUNKED_ENCODING_SIZE, 1024);
+                invocation.property(ClientProperties.REQUEST_ENTITY_PROCESSING, RequestEntityProcessing.CHUNKED);
+            }
+            response = invocation.method(request.getMethod().name(), getEntity());
+        }
+        else if(request.hasJson()) {
+            response = invocation.method(request.getMethod().name(), Entity.entity(request.getJson(), ClientConstants.CONTENT_TYPE_JSON));
+        }
+        else
+        {
+            response = invocation.method(request.getMethod().name());
+        }
+        
+        return response;
     }
-
+    
     private boolean isInputStreamEntity() {
-	return (hasEntity() && InputStream.class.isAssignableFrom(entity.getEntity().getClass()));
+        return (hasEntity() && InputStream.class.isAssignableFrom(entity.getEntity().getClass()));
     }
 
     /**
      * @return the request entity or null
      */
     public Entity<?> getEntity() {
-	return entity;
+        return entity;
     }
 
     /**
      * @return true if a request entity has been set
      */
     public boolean hasEntity() {
-	return entity != null;
+        return entity != null;
     }
-
+    
     /**
      * @return current retry execution count for this command
      */
     public int getRetries() {
-	return retries;
+        return retries;
     }
-
+    
     /**
      * @return incremement's the retry count and returns self
      */
     public HttpCommand<R> incrementRetriesAndReturn() {
-	initialize();
-
-	// Issue #676: ensure that input stream is resetted
-	if (hasEntity()) {
-	    if (isInputStreamEntity()) {             
-		try {
-		    ((InputStream) entity.getEntity()).reset();
-		} catch (IOException e) {
-		    throw new ConnectionException("Reset of input stream not possible. Aborting operation.", 0, e);
-		}
-	    }
-	}
-
-	retries++;
-	return this;
+    	initialize();
+    	
+    	// Issue #676: ensure that input stream is read again from start
+    	if (hasEntity()) {
+    	    if (isInputStreamEntity()) {             
+    	        try {
+    	            ((InputStream) entity.getEntity()).reset();
+    	        } catch (IOException e) {
+    	            throw new ConnectionException("Reset of input stream not possible. Aborting operation.", 0, e);
+    	        }
+    	    }
+    	}
+    		
+    	retries++;
+    	return this;
     }
 
     public HttpRequest<R> getRequest() {
-	return request;
+        return request;
     }
 
     private WebTarget populateQueryParams(WebTarget target, HttpRequest<R> request) {
 
-	if (!request.hasQueryParams()) return target;
+        if (!request.hasQueryParams()) return target;
 
-	for(Map.Entry<String, List<Object> > entry : request.getQueryParams().entrySet()) {
-	    for (Object o : entry.getValue()) {
-		target = target.queryParam(entry.getKey(), o);
-	    }
-	}
-	return target;
+        for(Map.Entry<String, List<Object> > entry : request.getQueryParams().entrySet()) {
+            for (Object o : entry.getValue()) {
+                target = target.queryParam(entry.getKey(), o);
+            }
+        }
+        return target;
     }
 
     private void populateHeaders(Invocation.Builder invocation, HttpRequest<R> request) {
 
-	if (!request.hasHeaders()) return;
+        if (!request.hasHeaders()) return;
 
-	for(Map.Entry<String, Object> h : request.getHeaders().entrySet()) {
-	    invocation.header(h.getKey(), h.getValue());
-	}
+        for(Map.Entry<String, Object> h : request.getHeaders().entrySet()) {
+            invocation.header(h.getKey(), h.getValue());
+        }
     }
 }
